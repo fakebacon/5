f875b36c95c475fe6c735b373650b2bad816d6ba
fakebacon <44483940+fakebacon@users.noreply.github.com>
2018-10-30 12:42:40 -0500

    Update and rename bpop.min.js to bpop.js

---
+++ bpop.js
@@ -0,0 +1,13 @@
+(function($){
+  jQuery(document).ready(function(){
+    jQuery('.main-navigation-toggle').attr('href', '#masthead');
+    function bellowPop(){jQuery('body').addClass('freeze');jQuery('.bellows, #primary, #secondary').addClass('fly');}
+    function bellowDown(){jQuery('body').removeClass('freeze');jQuery('.bellows, #primary, #secondary, .main-navigation-toggle').removeClass('fly').removeClass('active');window.history.pushState("", null, "./");}
+    jQuery('.main-navigation-toggle').on('click',function(){jQuery(this).toggleClass('fly');if(jQuery(this).is('.fly')){
+                bellowPop();
+                if (window.history && window.history.pushState) {window.history.pushState('TRAVEL', null, './#TRAVEL');
+                    jQuery(window).on('popstate', function() {bellowDown();});}
+    			jQuery('.bellows-main').not('.bellows-nav').on('click', function(){bellowDown();});}
+            if(jQuery(this).is(':not(.fly)')){bellowDown();}
+    }); 
+})(jQuery);
